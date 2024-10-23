4f3af92b5c8ee168a8050fc8b289c013dbd12907
GoblinQueen <marlie.ellingson@icloud.com>
2024-10-22 19:01:17 -0700

    I just finished game manager, added health

--- Assets/Scripts/Controller Class/Player Controller.cs
+++ Assets/Scripts/Controller Class/Player Controller.cs
@@ -10,11 +10,20 @@ public class PlayerController : Controller
     public KeyCode rotateClockwisekey;
     public KeyCode rotateCounterClockwisekey;
 
-
     // Start is called before the first frame update
     public override void Start()
     {
-        
+        // If we have a GameManger
+        if (GameManager.Instance != null)
+        {
+            // And it tracking our players in a list
+            if (GameManager.Instance.players != null)
+            {
+                // Register ourselfs with the GameManager
+                GameManager.Instance.players.Add(this);
+            }
+        }
+        base.Start();
     }
 
     // Update is called once per frame
@@ -22,7 +31,6 @@ public class PlayerController : Controller
     {
         ProcessInputs();
     }
-
 
     public override void ProcessInputs()
     {
@@ -44,6 +52,19 @@ public class PlayerController : Controller
         if (Input.GetKey(rotateCounterClockwisekey))
         {
             pawn.RotateCounterClockwise();
+        }
+    }
+    public void OnDestroy()
+    {
+        // If we have a GameManager
+        if (GameManager.Instance != null)
+        {
+            // And it tracks the players
+            if (GameManager.Instance.players != null)
+            {
+                // Remove Ourselves from the GameManager's list
+                GameManager.Instance.players.Remove(this);
+            }
         }
     }
 }
